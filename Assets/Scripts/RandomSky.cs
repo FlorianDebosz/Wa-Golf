666e82aca7b36c9ffcbe5facb5d9abfc86559233
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-11-09 12:05:35 +0100

    ✨ Camera now focused on ball (#1)

--- Assets/Scripts/RandomSky.cs
+++ Assets/Scripts/RandomSky.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RandomSky : MonoBehaviour
 {
     [SerializeField] private Material[] materials;
-    // Start is called before the first frame update
+
     void Start() {
            int random = Random.Range(0, materials.Length);
            RenderSettings.skybox = materials[random];
