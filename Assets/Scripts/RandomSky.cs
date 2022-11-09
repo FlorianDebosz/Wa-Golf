30943eaee9822fb186b3e0b055d9637a3263d9d0
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-11-09 11:08:27 +0100

    ✨ Animate Skybox

--- Assets/Scripts/RandomSky.cs
+++ Assets/Scripts/RandomSky.cs
@@ -6,9 +6,12 @@ public class RandomSky : MonoBehaviour
 {
     [SerializeField] private Material[] materials;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
            int random = Random.Range(0, materials.Length);
            RenderSettings.skybox = materials[random];
+    }
+
+    void Update() {
+        RenderSettings.skybox.SetFloat("_Rotation", 0.8f * Time.time);
     }
 }
