e4dde243fa3047e8cda9cd82df59c23444406a67
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2023-01-05 14:27:12 +0100

    :art: Shooting fixing slowness guideline and shotbutton

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -7,22 +7,72 @@ public class Shot : MonoBehaviour
 {
     [SerializeField] private RectTransform powerBar;
     [SerializeField] private bool powerActivate  = false;
-    [SerializeField] private float time = 0.1f;
+    [SerializeField] private float value = 0.1f;
+    [SerializeField] private float multiplicatedStrength = 100;
+    [SerializeField] private GameObject ball;
+    [SerializeField] private bool canShot = true;
+    [SerializeField] private GameObject shotButton;
+    [SerializeField] private GameObject GuideLine;
 
+    private bool checkSpeed = false;
+    
+    private void Awake() {
+        #if (UNITY_EDITOR || UNITY_STANDALONE)
+            shotButton.SetActive(false);
+        #endif
+    }
+    private void Update() {
+        if(Input.GetMouseButtonUp(1)){
+            HandleShot();
+        }
+    }
+
+    private void FixedUpdate() {
+        if(ball.GetComponent<Rigidbody>().velocity.magnitude <= 0.2f && checkSpeed){
+            canShot = true;
+        }    
+    }
+
+    public void HandleShot() {
+        if(canShot) {
+            if(!powerActivate) {
+                GuideLine.SetActive(true);
+                ActivatePowerBar();
+            }else {
+                canShot = false;
+                ShotBall();
+                GuideLine.SetActive(false);
+            }
+        }
+
+    }
     public void ActivatePowerBar() {
         powerActivate = true;
         StartCoroutine("AnimatePowerBar");
     }
 
+    public void ShotBall() {
+        powerActivate = false;
+        StopAllCoroutines();
+        float shotPower = powerBar.localScale.x * multiplicatedStrength;
+        ball.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * shotPower);
+        StartCoroutine("ActivateCheckSpeed");
+    }
+
+    IEnumerator ActivateCheckSpeed(){
+        yield return new WaitForSeconds(1);
+        checkSpeed = true;
+    }
+
     IEnumerator AnimatePowerBar() {
-        while (powerActivate == true) {
+        while (powerActivate) {
             yield return new WaitForSeconds(0.1f);
-            powerBar.localScale = new Vector3(powerBar.localScale.x - time,powerBar.localScale.y,powerBar.localScale.z);
-            if (powerBar.localScale.x < 0.1f) {
-                time = -time;
-            }else if(powerBar.localScale.x > 0.9f){
-                time = -time;
+            powerBar.localScale = new Vector3(powerBar.localScale.x - value,powerBar.localScale.y,powerBar.localScale.z);
+            if (powerBar.localScale.x <= 0.1f) {
+                value = -0.1f;
+            }else if(powerBar.localScale.x >= 1f){
+                value = 0.1f;
             }
-            }
+        }
     }
 }
